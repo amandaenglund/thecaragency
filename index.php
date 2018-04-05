4e12dae7dfeff18d4aa76462349e9419ffc52836
amandaenglund <32718487+amandaenglund@users.noreply.github.com>
2018-04-05 15:25:16 +0200

    har börjat designa layout på index

--- index.php
+++ index.php
@@ -6,13 +6,26 @@
     <title>The Car Agency</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <script src="script.js"></script>
 </head>
 <body>
 <header>
-
-
+<img src="./images/knesla_logo.png" alt="Knesla Logo" width="150px;">
 </header>
+
+
+<!--Navigationskategorierna kommer sedan att importeras från Json eller databasen-->
+<nav>
+Model S <!--bara placeholder för nu-->
+Model X <!--bara placeholder för nu-->
+Model 3 <!--bara placeholder för nu-->
+Sport <!--bara placeholder för nu-->
+</nav>
+
+
+
+
 <div id="content">
 
 <?php
@@ -24,6 +37,7 @@ echo "Hello World!";
 </div>    
 <footer>
 
+footer
 
 </footer>
 </body>

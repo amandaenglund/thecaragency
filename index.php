a32b88cfb0530ecc27bb8a5b182ac2c5c210dce7
amandaenglund <32718487+amandaenglund@users.noreply.github.com>
2018-04-05 15:47:26 +0200

    Påbörjat header

--- index.php
+++ index.php
@@ -5,20 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>The Car Agency</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="./styles/style.css" />
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <script src="script.js"></script>
 </head>
 <body>
 <header>
-<img src="./images/knesla_logo.png" alt="Knesla Logo" width="150px;">
+    <ul>
+        <li><img src="./images/knesla_logo.png" alt="Knesla Logo" width="150px;"></li>
+        <li><i class="fas fa-user-circle"></i> Hej Pedram</li>
+        <li><i class="fas fa-user-circle"></i> Mitt konto</li>
+        <li><i class="fas fa-shopping-cart"></i> (0)</li>
+    </ul>
+
 </header>
 
 
 <!--Navigationskategorierna kommer sedan att importeras från Json eller databasen-->
 <nav>
-Model S <!--bara placeholder för nu-->
-Model X <!--bara placeholder för nu-->
+Model 1 <!--bara placeholder för nu-->
+Model 2 <!--bara placeholder för nu-->
 Model 3 <!--bara placeholder för nu-->
 Sport <!--bara placeholder för nu-->
 </nav>
@@ -40,5 +46,10 @@ echo "Hello World!";
 footer
 
 </footer>
+
+
+<!-- Optional JavaScript -->
+<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+
 </body>
 </html>

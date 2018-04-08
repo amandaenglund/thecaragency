9683fbd714db659ffe8206b5eb038ee8f5d6e949
pedramamiri <32764741+pedramamiri@users.noreply.github.com>
2018-04-08 15:00:07 +0200

    Database && Admin classes

---
+++ classes/databas.php
@@ -0,0 +1,59 @@
+<?php
+/** Database connection manager */
+class Database {
+    protected $conn;
+    
+    protected function preQuery() {
+        if ($this->conn == null) {
+            $this->conn = new mysqli('localhost', 'amandaenglund', '}u0Nr%a1Z1Ox','amandaenglund' );
+
+            if ($this->conn->connect_error) {
+                die("Connection failed: " . $this->conn->connect_error);
+            } 
+        }
+    }
+
+    public function selectTabel() {
+        $this->preQuery();
+        $results = $this->conn->query("SELECT * FROM $this->tabel");
+        $tabel = [];
+        $object = $results->fetch_object();
+
+        while( $object != null ) {
+            array_push($tabel, $object);
+            $object = $results->fetch_object();
+        }
+        $this->connClose();
+        return $tabel;
+    }
+
+
+    
+            
+    protected function connClose() {
+        $this->conn->close();
+    }
+    
+    
+}
+// Class Admin
+class Admin extends Database {
+    
+    protected $tabel = 'admins';
+
+    public function login($email,$password) {
+        $this->preQuery();
+        $query = "SELECT email FROM";
+        $query .= " $this->tabel";
+        $query .= " WHERE email =";
+        $query .= " '".$email."' AND";
+        $query .= " password = '".$password."'";
+        $results = $this->conn->query($query);
+        $row = $results->fetch_assoc();
+        $this->connClose();
+        return $row;
+    }
+
+  
+}
+?>

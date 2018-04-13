78e168f45faf1ef9a42cbd0792376cba5a029a42
pedramamiri <32764741+pedramamiri@users.noreply.github.com>
2018-04-13 12:46:37 +0200

    customer server

---
+++ server/customer.php
@@ -0,0 +1,16 @@
+<?php
+if(!isset($_POST['action'])) die('Ett fel uppstod!');
+
+require('../classes/database.php');
+require('../classes/customer.php');
+$customer = new Customer;
+
+if(($_POST['action'] == 'CSIGNIN') && isset($_POST['email']) && isset($_POST['password'])) { 
+    
+    if($customer->signIn($_POST['email'], $_POST['password'])){ 
+    echo 1;
+    }else {echo 0;}   
+} 
+
+
+?>

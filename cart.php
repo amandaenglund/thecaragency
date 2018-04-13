a9f9730fda2cdd1a1168cac9b93ce93209e977ce
amandaenglund <32718487+amandaenglund@users.noreply.github.com>
2018-04-13 13:29:20 +0200

    Cart sida upplagd

---
+++ cart.php
@@ -0,0 +1,16 @@
+<?php
+    require('header.php');
+?>
+
+
+<?php
+// $query = "SELECT * FROM Products ORDER BY id ASC";
+// echo $query;
+// //$result = mysqli_query($connect, $query);
+?>
+
+
+
+<?php 
+    require('footer.php');
+?>

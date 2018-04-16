6dcb30daddf0f44695affdc11538131c0d49ca0a
amandaenglund <32718487+amandaenglund@users.noreply.github.com>
2018-04-16 12:21:30 +0200

    mer css

--- index.php
+++ index.php
@@ -5,15 +5,13 @@ require ('header.php');
     <a name="topSite"></a> 
     <h1 class="punchLine">KNESLA</h1>
     <ul class="bildform">   
-    <li><img class="bild" src="images/knesla_background.jpg"></img>
-    <img class="bild" src="images/1007.jpg"></img>
-    <img class="bild" src="images/1001.jpg"></img>
-    <img class="bild" src="images/1002.jpg"></img>
-    <img class="bild" src="images/1003.jpg"></img>
-    <img class="bild" src="images/1004.jpg"></img>
-    <img class="bild" src="images/1005.jpg"></img>
-</li>   
-</ul>
+        <img class="bild" src="images/1007.jpg"></img>
+        <img class="bild" src="images/1001.jpg"></img>
+        <img class="bild" src="images/1002.jpg"></img>
+        <img class="bild" src="images/1003.jpg"></img>
+        <img class="bild" src="images/1004.jpg"></img>
+        <img class="bild" src="images/1005.jpg"></img> 
+    </ul>
 </div>
 <?php 
 require ('footer.php');
